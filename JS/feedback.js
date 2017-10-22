42c2782efd34fd6745dfe9c49d692ea7d9435f55
obed123456 <31744395+obed123456@users.noreply.github.com>
2017-10-22 22:22:11 +0200

    connect feeback send button to feedback.js

--- JS/feedback.js
+++ JS/feedback.js
@@ -1,8 +1,9 @@
-function atten(form) {
-  if (onclick) {
-    document.getElementById("submit_message").innerHTML = "Tack!";
-    document.getElementById("main-content-dashboard").style.display ="none";
-  // } else {
-  //   document.getElementById("submit_message").innerHTML = "Välj 1 till 4:";
-  // }
+function allFeedback() {
+	if (document.getElementById('name_required').value === "") {
+		document.getElementById("fill_your_name").innerHTML ="Ange ditt namn.";
+	} else {
+    document.getElementById("submit_message").innerHTML = "Tack för din feedback!";
+    x = document.getElementsByClassName("main-content-column-left");
+    x[0].style.display ="none";
+  }
 }
