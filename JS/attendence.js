0c008cb190573b715472a1dfa8211b78461ab2ea
obed123456 <31744395+obed123456@users.noreply.github.com>
2017-10-24 11:16:07 +0200

    scroll-y, fixing disable after submit or atten is true

--- JS/attendence.js
+++ JS/attendence.js
@@ -5,7 +5,10 @@ var attendence = {
 function atten(form) {
   if (form.id.value == attendence.token) {
     document.getElementById("valid_message").innerHTML = "Tack! Närvaro skickat.";
-    document.getElementById("login").style.display ="none";
+    form.id.value = '';
+    // document.getElementById("login").style.display ="none";
+    document.getElementById("atten_btn").disabled = true;
+
   } else {
     document.getElementById("enterkod_code_message").innerHTML = "Fel inmattning kontakta din lärare";
   }
