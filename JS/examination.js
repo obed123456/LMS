b0a8a7ba978330b3ed68dede79a143ac1de86698
obed123456 <31744395+obed123456@users.noreply.github.com>
2017-11-01 15:58:34 +0100

    Add js for input for dev-teacher-course edit

---
+++ JS/examination.js
@@ -0,0 +1,17 @@
+    function addExcercise() {
+      var date = document.getElementById("date-input").value;
+      var excercise = document.getElementById("excercise-input").value; 
+      var description = document.getElementById("description-input").value;
+
+      if(description === "" || excercise === null || date === null) {
+         document.getElementById("invalid-message").innerHTML = "Ange datum och rubrik"; 
+         document.getElementById("invalid-message").style.color  ="red";   
+
+      } else {
+        document.getElementById("submit-message").innerHTML = "Sparat"; 
+        document.getElementById("invalid-message").style.display  ="none";  
+        document.getElementById("description-input").value ="";
+        document.getElementById("date-input").value = "";
+ 
+  }
+}
