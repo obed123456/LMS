5a099de42b580dd76798d622427d788fdac84313
Joakim Luong <joakimluong@Joakims-MacBook-Pro.local>
2017-11-27 11:10:45 +0100

    site validation

--- JS/modal.js
+++ JS/modal.js
@@ -22,9 +22,34 @@ function buttonSubmitClicked(event) {
     if (!document.getElementById("uploadBox").value) { //check if not file is selected
       event.preventDefault();
       alert("Please choose a file!");
-    } else {  
-    console.log("Skickat!");         
+    } else {
+    console.log("Skickat!");
     document.getElementById("success-message").innerHTML = "Filen är skickat!"; //varning this is not working
     alert("Filen är skickat");
     }
+  }
+
+
+  //---------------- comment button -------------------
+
+  // Get the modal
+  var modal = document.getElementById('id01');
+  var span = document.getElementsByClassName('close')[0];
+  // Get the button that opens the modal
+
+  	function submit(data) {
+  		modal.style.display = "block";
+  	}
+
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
   }
