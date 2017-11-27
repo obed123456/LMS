43b3409d74bbe8017633a5fd1e92dc076e7b7a8c
student899 <elias.liljegard@student.kyh.se>
2017-11-27 14:42:40 +0100

    fix2

--- JS/modal-studentlist.js
+++ JS/modal-studentlist.js
@@ -23,17 +23,48 @@ function newFunc2() {
 
 
 }
-//---------------- comment button -------------------
-
 // Get the modal
 var modal = document.getElementById('myModal');
-var span = document.getElementsByClassName('close')[0];
+
 // Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
+var btn6 = document.getElementById("myBtn6");
+var btn7 = document.getElementById("myBtn7");
+var btn8 = document.getElementById("myBtn8");
 
-	function comments(data) {
-		modal.style.display = "block";
-	}
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+}
+btn3.onclick = function() {
+    modal.style.display = "block";
+}
+btn4.onclick = function() {
+    modal.style.display = "block";
+}
+btn5.onclick = function() {
+    modal.style.display = "block";
+}
+btn6.onclick = function() {
+    modal.style.display = "block";
+}
+btn7.onclick = function() {
+    modal.style.display = "block";
+}
+btn8.onclick = function() {
+    modal.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
